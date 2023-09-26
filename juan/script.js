72e055d5ac899b35f6b9769465afaf23f29ddf9d
Lukas Escobar <134743612+Lescobar2904@users.noreply.github.com>
2023-09-25 23:35:06 -0300

    juan

---
+++ juan/script.js
@@ -0,0 +1,12 @@
+const barrasInput = document.getElementById("Barras");
+const instruccion = document.getElementById("Instruccion");
+
+barrasInput.addEventListener("keydown", function(event) {
+    if (event.keyCode === 13) { 
+        instruccion.textContent = "Espere, por favor";
+        setTimeout(function() {
+            instruccion.textContent = "Ha sido registrada su asistencia";
+            barrasInput.value = ""; 
+        }, 4000); 
+    }
+});
