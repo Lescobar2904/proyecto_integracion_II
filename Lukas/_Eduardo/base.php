484a28a94e05572fef672641ae2a249d2d6b3b89
Lukas Escobar <134743612+Lescobar2904@users.noreply.github.com>
2023-10-03 11:27:12 -0300

    cambios

--- Lukas/_Eduardo/base.php
+++ Lukas/_Eduardo/base.php
@@ -1,8 +1,7 @@
 <?php
-//Este programa PHP debe guardarse con el nombre:  acualizaPobla.php
 
 //Me conecto al motor de BD que corre en localhost, con el usuario root (sin password), y selecciono la BD del proyecto
- $bd = mysqli_connect("localhost","root", "","base de datos");
+ $bd = mysqli_connect("127.0.0.1","root@localhost", "","base de datos");
 
  $nom  = $_GET["nom"];
  $corr =  $_GET["Mail"];
