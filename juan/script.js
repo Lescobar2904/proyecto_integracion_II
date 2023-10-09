c546b48a776979df432c9eec1e731543aaa2dac7
TsuKenZo <128194754+TsuKenZo@users.noreply.github.com>
2023-10-08 21:37:02 -0300

    Update script.js

--- juan/script.js
+++ juan/script.js
@@ -1,23 +1,48 @@
-const barrasInput = document.getElementById("Barras");
-const instruccion = document.getElementById("Instruccion");
+function escanear() {
+    const codigoBarras = document.getElementById('codigoBarras').value;
 
-barrasInput.addEventListener("keydown", function(event) {
-    if (event.keyCode === 13) { 
-        instruccion.textContent = "Espere, por favor";
-        setTimeout(function() {
-            instruccion.textContent = "Ha sido registrada su asistencia";
-            barrasInput.value = ""; 
-        }, 4000); 
+    if (codigoBarras.trim() === '') {
+        mostrarMensaje('Por favor, ingrese un código de barras válido.', 'error');
+        return;
     }
-});
+
+    fetch('guardar.php', {
+        method: 'POST',
+        body: JSON.stringify({ codigoBarras }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.error) {
+            mostrarMensaje(data.error, 'error');
+        } else {
+            mostrarMensaje(data.message, 'success');
+            document.getElementById('codigoBarras').value = ''; 
+        }
+    })
+    .catch(error => {
+        mostrarMensaje('Ha ocurrido un error en la solicitud.', error);
+    });
+}
+
+function mostrarMensaje(mensaje, tipo) {
+    const mensajeElement = document.getElementById('mensaje');
+    mensajeElement.innerHTML = mensaje;
+    mensajeElement.className = tipo;
+}
 
 document.addEventListener("DOMContentLoaded", function() {
     var asis = document.getElementById("btn");
     asis.addEventListener("click", function() {
-        window.location.href = "Lukas/pag1.html";
+        window.location.href = "../pag1.HTML";
     });
 });
 
+window.addEventListener('load', function () {
+    document.body.classList.add('loaded');
+});
 
 
 
