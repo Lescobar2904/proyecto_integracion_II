5a7ff1e239b4f27a36cf4b60667ea1e4e34e7943
TsuKenZo <128194754+TsuKenZo@users.noreply.github.com>
2023-11-20 19:30:42 -0300

    Update script.js

--- _Juan/script.js
+++ _Juan/script.js
@@ -1,3 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const CodigoBarras = document.getElementById('codigoBarras');
+
+    CodigoBarras.focus();
+
+    var asis = document.getElementById('btn');
+    asis.addEventListener('click', function() {
+        window.location.href = 'https://pillan.inf.uct.cl/~lescobar/DesWeb/TallerIntegracion/Lukas/pag1.HTML';
+    });
+
+    window.addEventListener('load', function () {
+        document.body.classList.add('loaded');
+    });
+});
+
 function escanear() {
     const codigoBarras = document.getElementById('codigoBarras').value;
 
@@ -32,14 +47,3 @@ function mostrarMensaje(mensaje, tipo) {
     mensajeElement.textContent = mensaje;
     mensajeElement.className = tipo;
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-    var asis = document.getElementById("btn");
-    asis.addEventListener("click", function() {
-        window.location.href = "https://pillan.inf.uct.cl/~lescobar/DesWeb/TallerIntegracion/Lukas/pag1.HTML";
-    });
-});
-
-window.addEventListener('load', function () {
-    document.body.classList.add('loaded');
-});
